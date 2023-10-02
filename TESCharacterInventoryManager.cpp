6db2c0e2b1d5d8fbf3cbcb415e5df192907fd5ab
lighchu10 <36091832+lighchu10@users.noreply.github.com>
2023-10-02 19:08:19 +0900

    Add files via upload

---
+++ TESCharacterInventoryManager.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "Contents/CharacterInventory/Manager/TESCharacterInventoryManager.h"
+#include "Contents/CharacterInventory/UI/TESUWCharacterInventoryMain.h"
+#include "Contents/Conversation/Character/TESCharacterConversation.h"
+#include "Contents/Common/Manager/TESPopupManager.h"
+#include "TESGameInstance.h"
+#include "TESUtils.h"
+
+void UTESCharacterInventoryManager::Start()
+{
+	Super::Start();
+}
+
+void UTESCharacterInventoryManager::Reset()
+{
+	Super::Reset();
+
+}
+
+void UTESCharacterInventoryManager::Shutdown()
+{
+	Super::Shutdown();
+}
+
+//void UTESCharacterInventoryManager::RefreshCharacterInventory()
+//{
+//	UTESUWCharacterInventoryMain* characterInventoryWidget = SharedPopupManager->FindPopupWidget<UTESUWCharacterInventoryMain>();
+//
+//	if (nullptr != characterInventoryWidget)
+//	{
+//		characterInventoryWidget->RereshCharacterData();
+//	}
+//}
