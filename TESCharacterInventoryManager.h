34e352d653b6e1d4cfcf49f70d0eb8e42ba3e730
lighchu10 <36091832+lighchu10@users.noreply.github.com>
2023-10-02 19:08:08 +0900

    Add files via upload

---
+++ TESCharacterInventoryManager.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "TES.h"
+#include "Structs/Manager/TESManagerBase.h"
+#include "TESCharacterInventoryManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TES_API UTESCharacterInventoryManager : public UTESManagerBase
+{
+	GENERATED_BODY()
+
+//. ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+//. [ Override Functions ]
+//. ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────	
+public:
+	virtual void Start() override;
+	virtual void Reset() override;
+	virtual void Shutdown() override;
+
+//. ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+//. [ Memeber Variables ]
+//. ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+//. public:
+//. 	void RefreshCharacterInventory();
+//. ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+};
